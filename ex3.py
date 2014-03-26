8c7b44c049606652fe5a0d11a1d1144698b3d615
Radhika Morabia <hi@rmorabia.com>
2014-03-26 16:42:31 -0700

    Exercise 3
    Completion of Exercise 3

---
+++ ex3.py
@@ -0,0 +1,59 @@
+print "+ plus = addition"
+print "- minus = subtraction"
+print "/ slash = division"
+print "* asterisk = multiplication"
+print "% percent = x / y r(emainder) j. j is the result."
+print "< less-than = true/false"
+print "> greater-than = true/false"
+print "<= less-than-equal = true/false"
+print ">= greater-than-equal = true/false"
+
+print "..."
+
+# Prints words in ""
+print "I will now count my chickens:"
+
+# Prints words in "", then prints the results of the operation
+print "Hens", 25 + 30.0 / 6
+# Prints words in "", then prints the results of the operation
+print "Roosters", 100 - 25 * 3 % 4
+
+# Prints words in ""
+print "Now I will count the eggs:"
+
+# Prints result of the operation
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+# Prints words in ""
+print "Is it true that 3 + 2 < 5 - 7?"
+
+# Prints result of the operation
+print 3 + 2 < 5 - 7
+
+# Prints words in "", then prints the results of the operation
+print "What is 3 + 2?", 3 + 2
+# Prints words in "", then prints the results of the operation
+print "What is 5 - 7?", 5 - 7
+
+# Prints words in ""
+print "Oh, that's why it's False."
+
+# Prints words in ""
+print "How about some more."
+
+# Prints words in "", then prints the results of the operation
+print "Is it greater?", 5 > -2
+# Prints words in "", then prints the results of the operation
+print "Is it greater or equal?", 5 >= -2
+# Prints words in "", then prints the results of the operation
+print "Is it less or equal?", 5 <= -2
+
+print "..."
+
+print "How many visits to my website would I get per month if each day's statistics were the sameas today?"
+print 18 * 30.0
+
+print "..."
+
+print 7.0 / 4.0
+print 7 / 4
